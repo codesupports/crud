17232089fd64c28d612e8ed1568164438c710b1a
codesupports <raj.mah55@gmail.com>
2024-02-03 21:08:19 +0530

    my-first-commit in this branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      Some test here
+      <div className='container-fluid'>
+        <Header />
+        <div className='container py-4'>
+          <div className='table-wrapper'>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Address</th>
+                  <th>Phone</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>John</td>
+                  <td>Doe</td>
+                  <td>john@example.com</td>
+                  <td>john@example.com</td>
+                  <td>john@example.com</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+
+
+const Header = ()=>{
+    return (
+        <>
+            <div className="row header-main">
+                <div className="col-md-12">
+                    <div className="container">
+                        dsdsds
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
